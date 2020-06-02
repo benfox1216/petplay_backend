04639eb25926bf2602df6c838670a00245a23b0f
Anthony Gomes <code@tonygom.es>
2020-06-02 13:40:18 -0600

    Update for figaro

--- app/services/park_service.rb
+++ app/services/park_service.rb
@@ -1,21 +1,38 @@
+require 'excon'
+require 'json'
+
 class ParkService
-  att_reader :location
-  
-  def initialize(location)
-    @location = location
+  attr_reader :by_zipcode
+
+  def initialize(params)
+    @origin = params[:zipcode]
+    @by_zipcode = get_nearby_parks
   end
-  
-  def google_parks
-    JSON.parse(response.body, symbolize: true)
+
+  def get_nearby_parks
+    fetch_parks_by_zipcode
   end
-  
+
   private
-  
+
+  def fetch_parks_by_zipcode
+    repackaged_hash_response
+  end
+
+  def repackaged_hash_response
+    { results: parsed_json[:results] }
+  end
+
+  def parsed_json
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def response
-    key = ENV[GOOGLE_API_KEY]
-    params = "input=#{@location}&inputtype=textquery&key=#{key}"
-    
-    uri = "/maps/api/place/findplacefromtext/json?#{params}"
-    Faraday.get("https://maps.googleapis.com/#{uri}")
+    Excon.post('https://maps.googleapis.com/maps/api/place/textsearch/json',
+               query: {
+                        key: ENV[:GOOGLE_MAPS_API_KEY],
+                        query: "parks+in+#{@origin}",
+                        max_results: '20'
+                      })
   end
 end
