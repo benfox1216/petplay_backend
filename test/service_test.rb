39d6e75cca1ed4e4e286163d0b6016459551f000
Stephanie Friend <stephaniefriend91@gmail.com>
2020-06-03 21:57:24 -0600

    Add webmock to stub out api call

--- test/service_test.rb
+++ test/service_test.rb
@@ -1,19 +1,17 @@
 require_relative 'test_helper'
-require 'dotenv'
-require './lib/park_service'
 
 class ServiceTest < MiniTest::Test
 
   def setup
-    Dotenv.load('environment_keys.env')
-    @park_list = ParkService.park_list('80112')
+    json_response = File.read('test/fixtures/park_info_by_zip.json')
+    stub_request(:get, "https://maps.googleapis.com/maps/api/place/textsearch/json").
+        to_return(status: 200, body: json_response)
+    @json = JSON.parse(json_response, symbolize_names: true)
   end
 
   def test_it_can_return_formatted_response
-    assert_instance_of Array, @park_list
-    assert_equal 20, @park_list.count
-
-    @park_list.each do |park|
+    assert_instance_of Array, @json
+    @json.each do |park|
       assert_instance_of Hash, park
       park.each do |key, value|
         assert_equal value, park[key]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
+require 'webmock/minitest'
